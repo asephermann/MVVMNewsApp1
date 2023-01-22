06316f2b99567c14f5083e7d087c659718102d9f
asep <asephnursalam@gmail.com>
2023-01-22 10:37:52 +0700

    Initial commit

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/utils/Constants.kt
@@ -0,0 +1,10 @@
+package com.androiddevs.mvvmnewsapp.utils
+
+class Constants {
+    companion object{
+        const val API_KEY = "faab5c2870c94e7a9f84d564e00d9eed"
+        const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
+    }
+}
